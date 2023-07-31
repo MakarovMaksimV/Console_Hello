0346405116d125f268ae5955641c8e98fcadac88
Maksim <mirror0791@mail.ru>
2023-08-01 00:17:46 +0300

    hello user update v1.2

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿//Приглашение к вводу данныч
+Console.Write("Введите имя ");
+// Ввод имя пользователя
+String username =Console.ReadLine(); 
+// Вывод данных в консоли
+Console.Write("Привет, ");
+Console.Write(username);
