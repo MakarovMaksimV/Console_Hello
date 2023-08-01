826b533275d8faab6f2fa9b26f2d726886212114
Maksim <mirror0791@mail.ru>
2023-08-01 22:23:39 +0300

    Example004DivRandom

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA = 12;
-double numberB = 5;
+﻿int numberA = new Random().Next(12,78); //целое число от 1 до 78
+Console.WriteLine (numberA);
+int numberB = new Random().Next(1,3); // целое число от 16 до 37
+Console.WriteLine (numberB);
 Console.WriteLine(numberA / numberB); 
